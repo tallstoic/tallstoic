0fd5827f696db1a2f1cca2034c8246477487de80
Ankur Seth <iankurseth@gmail.com>
2022-02-10 01:35:55 +0530

    c1 with cra, tailwind and cleanup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+
+function App () {
+	return (
+		<div className="App">
+			<div className="min-h-screen flex bg-red-400 text-white">
+				<div className="text-4xl m-auto">
+					<a href="https://github.com/tallstoic" className="bg-red-500 px-12 py-6 duration-300 hover:bg-red-600">tallstoic</a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
